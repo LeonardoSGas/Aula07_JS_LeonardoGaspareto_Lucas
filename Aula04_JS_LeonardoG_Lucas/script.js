99ea9d846479c85a4e073a878f87d5f9e2410d46
LeonardoSGas <leonardo.gaspareto@escola.pr.gov.br>
2024-09-27 08:43:45 -0300

    Add files via upload

---
+++ Aula04_JS_LeonardoG_Lucas/script.js
@@ -0,0 +1,15 @@
+//Laço while = equanto
+var x =0;
+document.write('<br>testando loop com com while');
+while(x<=10){
+    document.write('<br> O valor de x é:' +x);
+    x=x+1;
+}
+
+//Laço for = para
+
+document.write('<br>testando loop com for');
+while(x<=10){
+    document.write('<br> O valor de x é:' +x);
+    x=x+1;
+}
